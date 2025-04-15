7fd165a351b11b57ea0cea5e76bcf13311473e28
kmin1231 <kmin91143@gmail.com>
2025-04-16 04:01:32 +0900

    🌱 chore: reinitialize the spring boot project

---
+++ src/main/java/io/owlbrothers/owlplay/OwlplayApplication.java
@@ -0,0 +1,13 @@
+package io.owlbrothers.owlplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlplayApplication.class, args);
+	}
+
+}
